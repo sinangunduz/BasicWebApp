a7abe2c329aa244a66e67eae3cb3c9c660280165
ge34ret <ge34ret@mytum.de>
2022-07-13 14:14:14 +0300

    what is your name vol2

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "Sin10";
         } else if (query.contains("what is your name"))
             return "Sin10";
         else { // TODO extend the programm here , extend right here
