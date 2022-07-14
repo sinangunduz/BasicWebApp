2a9f33d5ec6dec3dd249982d6d05784284f57f91
ge34ret <ge34ret@mytum.de>
2022-07-14 14:09:42 +0300

    warmup

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,17 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Sin10";
-        } else if (query.contains("what is your name"))
-            return "Sin10";
-        else if ("which of the following numbers is the largest: 98, 203")
-            return "203";
-        else if ("which of the following numbers is the largest: 43, 57, 806, 241")
-            return "806";
-        else if ("what is 14 plus 6")
-            return "20";
-        else if (" what is 12 plus 18")
-            return "30";
+           return "Sinan";
         else { // TODO extend the programm here , extend right here
             return "";
         }
